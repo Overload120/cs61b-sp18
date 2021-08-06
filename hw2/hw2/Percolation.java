d7fc03c0908fdb63866c18f240cca0c75da3aa76
Overload120 <1433866618@qq.com>
2021-08-06 19:28:53 +0800

    hw2 finished

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -7,82 +7,89 @@ public class Percolation {
     private boolean[][] arrIsOpen;
     private int numOfOpenSites;
     int numOfBottomFills;
-    public Percolation(int N){
-        if(N<=0){
+
+    public Percolation(int N) {
+        if (N <= 0) {
             throw new java.lang.IllegalArgumentException();
         }
-        numOfOpenSites=0;
-        numOfBottomFills=0;
-        field=new WeightedQuickUnionUF(N*N+2);
-        arrIsOpen=new boolean[N][N];
-        for(int i=0;i<N;i+=1){
-            for (int j=0;j<N;j+=1){
-                arrIsOpen[i][j]=false;
+        numOfOpenSites = 0;
+        numOfBottomFills = 0;
+        field = new WeightedQuickUnionUF(N * N + 2);
+        arrIsOpen = new boolean[N][N];
+        for (int i = 0; i < N; i += 1) {
+            for (int j = 0; j < N; j += 1) {
+                arrIsOpen[i][j] = false;
             }
         }
     }                // create N-by-N grid, with all sites initially blocked
-    public void open(int row, int col){
-        if(row>=arrIsOpen.length||col>=arrIsOpen[0].length||row<0||col<0){
+
+    public void open(int row, int col) {
+        if (row >= arrIsOpen.length || col >= arrIsOpen[0].length || row < 0 || col < 0) {
             throw new java.lang.IndexOutOfBoundsException();
         }
-        if(isOpen(row,col)){
+        if (isOpen(row, col)) {
             return;
         }
-        arrIsOpen[row][col]=true;
-        if(row==0){
-            field.union(row*arrIsOpen.length+col,arrIsOpen.length*arrIsOpen.length);
+        arrIsOpen[row][col] = true;
+        if (row == 0) {
+            field.union(row * arrIsOpen.length + col, arrIsOpen.length * arrIsOpen.length);
         }
-        if(row==arrIsOpen.length-1&&numOfBottomFills==0){
-            field.union(row*arrIsOpen.length+col,arrIsOpen.length*arrIsOpen.length+1);
-            numOfBottomFills+=1;
+        if (row == arrIsOpen.length - 1 && numOfBottomFills == 0) {
+            field.union(row * arrIsOpen.length + col, arrIsOpen.length * arrIsOpen.length + 1);
+            numOfBottomFills += 1;
         }
-        numOfOpenSites+=1;
+        numOfOpenSites += 1;
         int currentRow;
         int currentCol;
         boolean rowIsOutside;
         boolean colIsOutside;
-        for(int i=1;i<=2;++i){
-            currentRow=row+(int) Math.pow(-1,i);
-            rowIsOutside=currentRow<0||currentRow>arrIsOpen.length-1;
-            if(rowIsOutside){
+        for (int i = 1; i <= 2; ++i) {
+            currentRow = row + (int) Math.pow(-1, i);
+            rowIsOutside = currentRow < 0 || currentRow > arrIsOpen.length - 1;
+            if (rowIsOutside) {
                 continue;
             }
-            if(arrIsOpen[currentRow][col]){
-                field.union(row*arrIsOpen.length+col,currentRow*arrIsOpen.length+col);
+            if (arrIsOpen[currentRow][col]) {
+                field.union(row * arrIsOpen.length + col, currentRow * arrIsOpen.length + col);
             }
         }
-        for(int i=1;i<=2;++i){
-            currentCol=col+(int) Math.pow(-1,i);
-            colIsOutside=currentCol<0||currentCol>arrIsOpen[0].length-1;
-            if(colIsOutside){
+        for (int i = 1; i <= 2; ++i) {
+            currentCol = col + (int) Math.pow(-1, i);
+            colIsOutside = currentCol < 0 || currentCol > arrIsOpen[0].length - 1;
+            if (colIsOutside) {
                 continue;
             }
-            if(arrIsOpen[row][currentCol]){
-                field.union(row*arrIsOpen.length+col,row*arrIsOpen.length+currentCol);
+            if (arrIsOpen[row][currentCol]) {
+                field.union(row * arrIsOpen.length + col, row * arrIsOpen.length + currentCol);
             }
         }
 
-        }
-          // open the site (row, col) if it is not open already
-    public boolean isOpen(int row, int col){
-        if(row>=arrIsOpen.length||col>=arrIsOpen[0].length||row<0||col<0){
+    }
+
+    // open the site (row, col) if it is not open already
+    public boolean isOpen(int row, int col) {
+        if (row >= arrIsOpen.length || col >= arrIsOpen[0].length || row < 0 || col < 0) {
             throw new java.lang.IndexOutOfBoundsException();
         }
         return arrIsOpen[row][col];
     }  // is the site (row, col) open?
-    public boolean isFull(int row, int col){
-        if(row>=arrIsOpen.length||col>=arrIsOpen[0].length||row<0||col<0){
+
+    public boolean isFull(int row, int col) {
+        if (row >= arrIsOpen.length || col >= arrIsOpen[0].length || row < 0 || col < 0) {
             throw new java.lang.IndexOutOfBoundsException();
         }
-        return field.connected(row*arrIsOpen.length+col,arrIsOpen.length*arrIsOpen.length);
+        return field.connected(row * arrIsOpen.length + col, arrIsOpen.length * arrIsOpen.length);
     }  // is the site (row, col) full?
-    public int numberOfOpenSites(){
+
+    public int numberOfOpenSites() {
         return numOfOpenSites;
     }           // number of open sites
-    public boolean percolates(){
-        return field.connected(arrIsOpen.length*arrIsOpen.length+1,arrIsOpen.length*arrIsOpen.length);
+
+    public boolean percolates() {
+        return field.connected(arrIsOpen.length * arrIsOpen.length + 1, arrIsOpen.length * arrIsOpen.length);
     }// does the system percolate?
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
 
     }   // use for unit testing (not required)
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -3,36 +3,40 @@ package hw2;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
-public class PercolationStats{
+public class PercolationStats {
     private double[] arrOfThreshold;
 
-    public PercolationStats(int N, int T, PercolationFactory pf){
-    if(N<=0||T<=0){
-        throw new java.lang.IllegalArgumentException();
-    }
-    arrOfThreshold=new double[T];
-    int randomRow;
-    int randomCol;
-    for (int i=0;i<T;i+=1){
-        Percolation test=pf.make(N);
-        while (!test.percolates()){
-            randomRow= StdRandom.uniform(N);
-            randomCol=StdRandom.uniform(N);
-            test.open(randomRow,randomCol);
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new java.lang.IllegalArgumentException();
         }
-        arrOfThreshold[i]=(double)test.numberOfOpenSites()/(N*N);
-    }
+        arrOfThreshold = new double[T];
+        int randomRow;
+        int randomCol;
+        for (int i = 0; i < T; i += 1) {
+            Percolation test = pf.make(N);
+            while (!test.percolates()) {
+                randomRow = StdRandom.uniform(N);
+                randomCol = StdRandom.uniform(N);
+                test.open(randomRow, randomCol);
+            }
+            arrOfThreshold[i] = (double) test.numberOfOpenSites() / (N * N);
+        }
     }   // perform T independent experiments on an N-by-N grid
-    public double mean(){
+
+    public double mean() {
         return StdStats.mean(arrOfThreshold);
     }                                // sample mean of percolation threshold
-    public double stddev(){
+
+    public double stddev() {
         return StdStats.stddev(arrOfThreshold);
     }                                         // sample standard deviation of percolation threshold
-    public double confidenceLow(){
-        return mean()-1.96*stddev()/Math.sqrt(arrOfThreshold.length);
+
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(arrOfThreshold.length);
     }                                  // low endpoint of 95% confidence interval
-    public double confidenceHigh(){
-        return mean()+1.96*stddev()/Math.sqrt(arrOfThreshold.length);
+
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(arrOfThreshold.length);
     }                                 // high endpoint of 95% confidence interval
 }
